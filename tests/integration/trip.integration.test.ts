f8cf14276e659c355bb6840c84abd059b0dccaa7
Mitchell Bird <mitchell.bird@live.co.uk>
2025-10-01 01:10:36 +0000

    added integration test without route returned

--- tests/integration/trip.integration.test.ts
+++ tests/integration/trip.integration.test.ts
@@ -2,7 +2,7 @@ import request from "supertest";
 import { app } from "../../src/api/server";
 
 describe("Integration: /api/trip", () => {
-  it("calculates a trip using real OSRM + Nominatim", async () => {
+  it("calculates a trip using real OSRM + Nominatim with route", async () => {
       const res = await request(app)
         .post("/api/trip")
         .send({
@@ -12,6 +12,29 @@ describe("Integration: /api/trip", () => {
           model_year: "2008",
           locations: ["Cuba Street, Wellington, New Zealand", "Te Papa Museum, Wellington, New Zealand"],
           overview: "full"
+        })
+        .set("Accept", "application/json");
+
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty("distance_km");
+      expect(res.body).toHaveProperty("hours");
+      expect(res.body).toHaveProperty("minutes");
+      expect(res.body).toHaveProperty("fuel_used_l");
+      expect(res.body).toHaveProperty("co2_kg");
+    },
+    20000
+  );
+
+  it("calculates a trip using real OSRM + Nominatim without route", async () => {
+      const res = await request(app)
+        .post("/api/trip")
+        .send({
+          vehicle_id: 1,
+          make: "Toyota",
+          model: "Corolla",
+          model_year: "2008",
+          locations: ["Cuba Street, Wellington, New Zealand", "Te Papa Museum, Wellington, New Zealand"],
+          overview: "false"
         })
         .set("Accept", "application/json");
 
