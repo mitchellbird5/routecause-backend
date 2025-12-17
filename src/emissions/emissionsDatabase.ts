e3ffa4aaa72c6ab7538efbf7f9454aa5c9e0b67a
Mitchell Bird <mitchell.bird@live.co.uk>
2025-12-17 03:37:10 +0000

    added ssl for supabase sql hosting

--- src/emissions/emissionsDatabase.ts
+++ src/emissions/emissionsDatabase.ts
@@ -6,6 +6,7 @@ const pool = new Pool({
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
+  ssl: { rejectUnauthorized: false }, // required for Supabase
 });
 
 export default pool;
